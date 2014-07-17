89db3d336b309579f6e56babd2e66ca73f094bec
dypa <dypa@bk.ru>
2014-07-17 13:25:58 +0400

    Queue accepts only BaseTask instance

--- src/Helper/Queue.php
+++ src/Helper/Queue.php
@@ -3,11 +3,16 @@
 namespace PhpMultiCurl\Helper;
 
 use SplQueue;
+use PhpMultiCurl\Task\BaseTask;
 
 class Queue extends SplQueue
 {
     public function enqueue($task)
     {
+        if (!($task instanceof BaseTask)) {
+            throw new Exception('Queue accepts only BaseTask instance');
+        }
+
         return parent::enqueue($task);
     }
 }
